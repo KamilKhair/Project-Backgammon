af238711a50c59d3aad5777120da9659227772cb
KamilKhair <KamilK@code-college.net>
2016-08-09 14:34:53 +0300

    Backgammon Project - Latest version

--- BackgammonLib/BackgammonLib/IChecker.cs
+++ BackgammonLib/BackgammonLib/IChecker.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BackgammonLib
+﻿namespace BackgammonLib
 {
     public interface IChecker
     {
